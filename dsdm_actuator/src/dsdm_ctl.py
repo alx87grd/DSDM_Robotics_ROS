694ce9cba25838ddd2eb34acd3f191a13ac6454b
Alex <alx87grd@gmail.com>
2016-08-10 19:29:47 -0400

    added speed estimation and feedback in dsdm node

--- dsdm_actuator/src/dsdm_ctl.py
+++ dsdm_actuator/src/dsdm_ctl.py
@@ -2,6 +2,7 @@
 import rospy
 import numpy as np
 from dsdm_msgs.msg import dsdm_actuator_control_inputs
+from dsdm_msgs.msg import dsdm_actuator_sensor_feedback
 from flexsea_execute.msg import inputs
 from flexsea_execute.msg import outputs
 from sensor_msgs.msg import Joy
@@ -21,6 +22,8 @@ class DSDM_CTL(object):
         
         self.pub_cmd_m1     = rospy.Publisher("M1/u", inputs , queue_size=1      )
         self.pub_cmd_m2     = rospy.Publisher("M2/u", inputs , queue_size=1      )
+        self.pub_y          = rospy.Publisher("y", dsdm_actuator_sensor_feedback , queue_size=1      )
+        
         self.sub_u          = rospy.Subscriber("u", dsdm_actuator_control_inputs , self.setpoint_callback , queue_size=1 )
         self.sub_y_m1       = rospy.Subscriber("M1/y", outputs , self.feedback_callback_m1 , queue_size=1 )
         self.sub_y_m2       = rospy.Subscriber("M2/y", outputs , self.feedback_callback_m2 , queue_size=1 )
@@ -30,12 +33,24 @@ class DSDM_CTL(object):
         self.k = 1
         
         # Feedback from sensors
-        self.y = [0,0]  # M1 = [0], M2 = [1]
-        self.w = [0,0]
+        self.y = np.zeros(3) # M1 = [1], M2 = [1]
+        self.w = np.zeros(3)  # Velocity
+        
+        # Memory for speed filter
+        self.y_past = np.zeros(3)   # t-1 value
+        self.w_past = np.zeros(3)  # t-1 value
+        self.w_x    = np.zeros(3)  # state of the filter
+        
+        self.dt = 0.02
+        RC      = 1.0
+        
+        self.alpha = self.dt / ( RC + self.dt )
+        print self.alpha
         
         
         #Motor signs: TODO read from params
         self.m_sign = [-1,1]
+        self.g      = [1,1,1]   # Gear ratios [output,M1,M2]
         
         
         # Init flexsea input msg
@@ -83,6 +98,7 @@ class DSDM_CTL(object):
     
     ############################################
     def setpoint_callback( self , msg ):
+        
         """ Record new setpoint """
         self.f = msg.f  # effort
         self.k = msg.k  # mode
@@ -91,25 +107,37 @@ class DSDM_CTL(object):
     def feedback_callback_m1( self , msg ):
         """ """
         
-        self.update_feedback( msg , 0 )
+        self.update_feedback( msg , 1 )
     
     ############################################
     def feedback_callback_m2( self , msg ):
         """ """
         
-        self.update_feedback( msg , 1 )
+        self.update_feedback( msg , 2 )
         
         # For asynchrone ctl
         self.controller()
+        self.pub_feedback( msg )
         
         
     ############################################
     def update_feedback( self , msg , motor_ID ):
         """ """
-        self.y[ motor_ID ] = msg.encoder
+        self.y_past[ motor_ID ] = self.y[ motor_ID ]
+        self.y[ motor_ID ]      = msg.encoder
         
         # TODO : FILTER diff for speed
-        self.w[ motor_ID ] = 0
+        dy = self.y[ motor_ID ] - self.y_past[ motor_ID ]
+        
+        w  = ( dy + 0.0)  / self.dt      
+        
+        w_filtered = self.alpha * self.w_x[ motor_ID ] + ( 1 - self.alpha ) * w
+        
+        self.w[ motor_ID ] = int( w_filtered )
+          
+        # Memory
+        self.w_x[    motor_ID ] = w_filtered
+        self.y_past[ motor_ID ] = self.y[ motor_ID ]
         
         
     ###########################################
@@ -135,6 +163,27 @@ class DSDM_CTL(object):
         
         self.pub_cmd_m1.publish( msg_m1 )
         self.pub_cmd_m2.publish( msg_m2 )
+        
+        
+    ###########################################
+    def pub_feedback( self , msg ):
+        """ """
+        
+        msg_y = dsdm_actuator_sensor_feedback()
+        
+        # Copy Header
+        
+        msg_y.header.stamp     = msg.header.stamp 
+        msg_y.header.frame_id  = msg.header.frame_id
+        
+        # Feedback info
+        
+        msg_y.theta = self.y
+        msg_y.w     = self.w
+        
+        msg_y.test   = self.w[2]
+        
+        self.pub_y.publish( msg_y )
         
         
 #########################################
