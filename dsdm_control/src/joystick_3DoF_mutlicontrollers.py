ce1a1759c57ed78b5ca814aa2e2d621e8f925b86
Alex <alx87grd@gmail.com>
2016-11-18 15:04:22 -0500

    added all control modes

--- dsdm_control/src/joystick_3DoF_mutlicontrollers.py
+++ dsdm_control/src/joystick_3DoF_mutlicontrollers.py
@@ -6,43 +6,34 @@ from std_msgs.msg    import Float64MultiArray, Bool, Int32
 from dsdm_msgs.msg   import dsdm_actuator_control_inputs
 
 from AlexRobotics.dynamic  import Prototypes             as Proto
+from AlexRobotics.control  import RminComputedTorque     as RminCTC
 from AlexRobotics.control  import RolloutComputedTorque  as RollCTC
 
 #########################################
 class Robot_controller(object):
     """
-    navigation
+    General class for controlling dsdm robot control
     """
     def __init__(self):
         
         self.verbose = False
         
+        # Suscribers
         self.sub_joy      = rospy.Subscriber("joy"   , Joy               , self.joy_callback   , queue_size=1 )
         self.sub_state    = rospy.Subscriber("x_hat" , Float64MultiArray , self.state_callback , queue_size=1 )
         
+        # Publishers
         self.pub_a0u      = rospy.Publisher("a0/u", dsdm_actuator_control_inputs , queue_size=1  )
         self.pub_a1u      = rospy.Publisher("a1/u", dsdm_actuator_control_inputs , queue_size=1  )
         self.pub_a2u      = rospy.Publisher("a2/u", dsdm_actuator_control_inputs , queue_size=1  )
         
-        
         # Load ROS params
         self.load_params( None )
         
-        
-        # Load robot model for the right configuration
-        if self.robot_config == 'wrist-only':
-            self.R = Proto.SingleRevoluteDSDM()
-            
-        elif self.robot_config == 'dual-plane' :
-            self.R = Proto.TwoPlanarSerialDSDM()
-            
-        else:
-            self.R = None
-            
         # Operating Modes
         self.enable = False
         self.mode   = 0
-        self.modes  = [ 'open_loop ' , 'acceleration' , 'speed' , 'position' , 'auto' ]
+        self.modes  = [ 'open_loop ' , 'acceleration' , 'speed' , 'position' , 'custom1' , 'custom2' , 'custom3' ]
         
         # Init DSDM msgs
         self.f = np.array([0.,0.,0.])
@@ -53,9 +44,20 @@ class Robot_controller(object):
         self.joy = Joy()                 # memory for last joy msg
         self.x   = np.zeros( self.R.n )  # Memory for robots state feedback
         
-        
         # Timers
         self.t_joy   =  rospy.get_rostime()  # last joy change
+        self.t_zero  =  rospy.get_rostime()
+        
+        # Init setpoints
+        
+        self.fd = np.array([0.,0.,0.])
+        self.kd = np.array([1 ,1 ,1 ]) # High force mode
+        self.nd = np.array([0.,0.,0.])
+        
+        self.ddq_d = np.zeros( self.R.dof )
+        self.dq_d  = np.zeros( self.R.dof )
+        self.q_d   = np.zeros( self.R.dof )
+        
         
     ###########################################
     def load_params(self, event):
@@ -63,7 +65,44 @@ class Robot_controller(object):
         
         self.robot_type     = rospy.get_param("robot_type"  ,  'pendulum'    )
         self.robot_config   = rospy.get_param("robot_config",  'wrist-only'  )
+        self.robot_ctl      = rospy.get_param("controller",  'RfixCTC'       )
+        self.fixed_mode     = rospy.get_param("fixed_mode",  1               )
         
+        
+        ###############################################
+        # Load robot model for the right configuration
+        if self.robot_config == 'wrist-only':
+            self.R = Proto.SingleRevoluteDSDM()
+            
+        elif self.robot_config == 'dual-plane' :
+            self.R = Proto.TwoPlanarSerialDSDM()
+            
+        else:
+            self.R = None
+            
+        ###############################################
+        # Load controller
+        if self.robot_ctl == 'RfixCTC' :
+            self.Ctl = RminCTC.RfixComputedTorqueController( self.R , self.fixed_mode )
+            
+        elif self.robot_ctl == 'RminCTC' :
+            self.Ctl = RminCTC.RminComputedTorqueController( self.R )
+            
+        elif self.robot_ctl == 'RfixSLD' :
+            self.Ctl = RminCTC.RfixSlidingModeController( self.R , self.fixed_mode )
+            
+        elif self.robot_ctl == 'RminSLD' :
+            self.Ctl = RminCTC.RminSlidingModeController( self.R )
+            
+        elif self.robot_ctl == 'RollCTC' :
+            self.Ctl = RollCTC.RolloutComputedTorqueController( self.R )
+            
+        elif self.robot_ctl == 'RollSLD' :
+            self.Ctl = RollCTC.RolloutSlidingModeController( self.R )
+            
+        else:
+            self.Ctl = None
+            
         
     #######################################   
     def joy_callback( self, msg ):
@@ -83,7 +122,7 @@ class Robot_controller(object):
         #####################
         # Update control mode
             
-        if ( ( t_now -  t_last ).to_sec() > 0.5 ) :  # Avoid double detection
+        if ( ( t_now -  t_last ).to_sec() > 0.2 ) :  # Avoid double detection
             if msg.buttons[13] :
                 self.mode = self.mode + 1
                 print('--------- Control Mode set to : ' + self.modes[ self.mode ]  + ' ----------------------')
@@ -91,11 +130,58 @@ class Robot_controller(object):
             elif msg.buttons[14] :
                 self.mode = self.mode - 1
                 print('--------- Control Mode set to : ' + self.modes[ self.mode ]  + ' ----------------------' )
+                
         
         #########################
         # Save last buttons states
         self.joy   = msg
         self.t_joy = t_now
+        
+                
+        ############################################
+        # update setpoints for the possible modes
+                
+        """ Open Loop Setpoints """
+        
+        """ Ball screw DoF """
+        #self.f  =    msg.axes[1] * 0.2
+        self.fd[0] = self.joy.buttons[4] * 0.2 + self.joy.buttons[5] * -0.2 
+            
+        # Pick mode with trigger
+        if ( self.joy.axes[5] < 0):
+            self.kd[0] = 0
+        else:
+            self.kd[0] = 1
+            
+        """ elbow """            
+        self.fd[1] = self.joy.axes[1] * 0.2
+        self.kd[1] = not( self.joy.buttons[0] )
+        self.nd[1] = self.joy.axes[0] * 0.5
+        
+        """ wrist """            
+        self.fd[2] = self.joy.axes[4] * 0.2
+        self.kd[2] = not( self.joy.buttons[1] )
+        self.nd[2] = self.joy.axes[3] * 0.5
+        
+        ###################
+        
+        if self.robot_config == 'wrist-only':
+            
+            self.ddq_d[0] = self.joy.axes[4] * np.pi * 10
+            self.dq_d[0]  = self.joy.axes[4] * np.pi * 1
+            self.q_d[0]   = self.joy.axes[4] * np.pi * 1
+            
+        elif self.robot_config == 'dual-plane' :
+            
+            self.ddq_d[0] = self.joy.axes[1] * np.pi * 10
+            self.dq_d[0]  = self.joy.axes[1] * np.pi * 1
+            self.q_d[0]   = self.joy.axes[1] * np.pi * 1
+            
+            self.ddq_d[1] = self.joy.axes[4] * np.pi * 10
+            self.dq_d[1]  = self.joy.axes[4] * np.pi * 1
+            self.q_d[1]   = self.joy.axes[4] * np.pi * 1
+                
+        
         
         
     #######################################   
@@ -123,39 +209,75 @@ class Robot_controller(object):
     def control_callback( self, msg ):
         """ Main control loop """
         
+        ######################################
+        
+        # State feedback
+        x             = self.x
+        [ q , dq ]    = self.R.x2q( self.x  )
+        
+        # Get time
+        t_ros = rospy.get_rostime() - self.t_zero
+        t     = t_ros.to_sec()
+        
         #######################################
         # Open Loop
         if ( self.mode == 0 ):
-            # Pick set_point with joysticks gain
-        
-            """ Ball screw DoF """
-            #self.f  =    msg.axes[1] * 0.2
-            self.f[0] = self.joy.buttons[4] * 0.2 + self.joy.buttons[5] * -0.2 
-                
-            # Pick mode with trigger
-            if ( self.joy.axes[5] < 0):
-                self.k[0] = 0
-            else:
-                self.k[0] = 1
-                
-            """ Shoulder """
+            # Pick commands with joysticks setpoints
             
-            self.f[1] = self.joy.axes[1] * 0.2
-            self.k[1] = not( self.joy.buttons[0] )
-            self.n[1] = self.joy.axes[0] * 0.5
-            
-            """ Elbow """
-            
-            self.f[2] = self.joy.axes[4] * 0.2
-            self.k[2] = not( self.joy.buttons[1] )
-
-            self.n[2] = self.joy.axes[3] * 0.5
+            self.f = self.fd
+            self.k = self.kd
+            self.n = self.nd
             
         #######################################
         # Closed Loop Acc
         elif ( self.mode == 1 ):
             
-            print self.x
+            self.Ctl.ddq_manual_setpoint = self.ddq_d
+            
+            u = self.Ctl.manual_acc_ctl(  x , t )
+            
+            self.u2fkn( u ) # convert to actuator cmds
+            
+            
+        #######################################
+        # Closed Loop Speed
+        elif ( self.mode == 2 ):
+            
+            self.Ctl.goal = self.R.q2x( q , self.dq_d )
+            
+            u = self.Ctl.fixed_goal_ctl( x  , t )
+            
+            self.u2fkn( u ) # convert to actuator cmds
+            
+            
+        #######################################
+        # Closed Loop Position
+        elif ( self.mode == 3 ):
+            
+            self.Ctl.goal = self.R.q2x( self.q_d , np.zeros( self.R.dof ) )
+            
+            u = self.Ctl.fixed_goal_ctl( x  , t )
+            
+            self.u2fkn( u ) # convert to actuator cmds
+            
+            
+        #######################################
+        # Custom 1
+        elif ( self.mode == 4 ):
+            
+            print x
+            
+        #######################################
+        # Custom 2
+        elif ( self.mode == 5 ):
+            
+            pass
+            
+        #######################################
+        # Custom 3
+        elif ( self.mode == 6 ):
+            
+            pass
         
         
         #######################################
@@ -173,8 +295,59 @@ class Robot_controller(object):
             
             print( 'Ctrl mode: ', self.k , ' Set point: ', self.f )
             
+    
+    #######################################   
+    def u2fkn( self , u ):
+        """ Convert input array into individual actuator commands """
+        
+        self.f = np.array([0.,0.,0.])
+        self.k = np.array([1 ,1 ,1 ]) # High force mode
+        self.n = np.array([0.,0.,0.])
+        
+        if self.robot_config == 'wrist-only':
             
-
+            """ wrist """            
+            self.f[2] = u[0]
+            self.n[2] = 0
+            
+            """ Dsicrete mode """
+            if u[1] == self.R.R[0] :
+                self.k[2] = 0
+            else:
+                self.k[2] = 1
+                
+            
+        elif self.robot_config == 'dual-plane' :
+            
+            """ elbow """            
+            self.f[1] = u[0]
+            self.n[1] = 0
+            
+            """ wrist """            
+            self.f[2] = u[1]
+            self.n[2] = 0
+            
+            """ Dsicrete mode """
+            if u[2] == 0 :
+                self.k[1] = 0
+                self.k[2] = 0
+                
+            elif u[2] == 1 :
+                self.k[1] = 0
+                self.k[2] = 1
+                
+            elif u[2] == 2 :
+                self.k[1] = 1
+                self.k[2] = 0
+                
+            else:
+                self.k[1] = 1
+                self.k[2] = 1
+            
+        
+        
+        
+        
     #######################################   
     def pub_u_msg( self ):
         """ pub actuator cmd """
